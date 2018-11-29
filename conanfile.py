8912844c918d502c99ae0a5c4b0f6ff8525297c5
cppbitman <daihongjun@kedacom.com>
2018-11-28 21:35:28 -0800

    linux build done

--- conanfile.py
+++ conanfile.py
@@ -1,44 +1,58 @@
-from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment
+from conans import ConanFile, CMake, tools, Meson
 from shutil import copyfile
 import os
+from conanos.build import config_scheme
 
 class FribidiConan(ConanFile):
     name = "fribidi"
-    version = "0.19.7"
+    version = "1.0.5"
     description = "GNU FriBidi is an implementation of the Unicode Bidirectional Algorithm"
     url = "https://github.com/conan-multimedia/fribidi"
     homepage = "http://fribidi.org/"
     license = "LGPLv2_1Plus"
+    generators = "visual_studio", "gcc"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
-    generators = "cmake"
-    requires = "libffi/3.3-rc0@conanos/dev","glib/2.58.0@conanos/dev"
-    source_subfolder = "source_subfolder"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = { 'shared': True, 'fPIC': True }
+
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
+
+    
+    def build_requirements(self):
+        self.build_requires("glib/2.58.1@conanos/stable")
+        self.build_requires("libffi/3.299999@conanos/stable")
+    
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+    
+    def configure(self):
+        del self.settings.compiler.libcxx
+
 
     def source(self):
-        tools.get('https://github.com/fribidi/fribidi/releases/download/{version}/{name}-{version}.tar.bz2'.format(name=self.name, version=self.version))
+        url_ = 'https://github.com/fribidi/fribidi/archive/v{version}.tar.gz'.format(version=self.version)
+        tools.get(url_)
         extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        os.rename(extracted_dir, self._source_subfolder)
 
     def build(self):
-        with tools.chdir(self.source_subfolder):
-            #self.run("autoreconf -f -i")
+        pkg_config_paths=[ os.path.join(self.deps_cpp_info[i].rootpath, "lib", "pkgconfig") for i in ["glib","libffi"] ]
+        prefix = os.path.join(self.build_folder, self._build_subfolder, "install")
+        meson = Meson(self)
 
-            _args = ["--prefix=%s/builddir"%(os.getcwd())]
-            if self.options.shared:
-                _args.extend(['--enable-shared=yes','--enable-static=no'])
-            else:
-                _args.extend(['--enable-shared=no','--enable-static=yes'])
-            
-            self.run('./configure %s'%(' '.join(_args)))
-            self.run('make -j4')
-            self.run('make install')
+        meson.configure(defs={'prefix' : prefix,'docs':'false','libdir':'lib'},
+                        source_dir=self._source_subfolder, build_dir=self._build_subfolder,
+                        pkg_config_paths=pkg_config_paths)
+        meson.build()
+        self.run('ninja -C {0} install'.format(meson.build_dir))
 
     def package(self):
-        if tools.os_info.is_linux:
-            with tools.chdir(self.source_subfolder):
-                self.copy("*", src="%s/builddir"%(os.getcwd()))
+        self.copy("*", dst=self.package_folder, src=os.path.join(self.build_folder,self._build_subfolder, "install"))
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
